0cf80ececccdd1e31d227858c4413e43cb1f6223
KryptonANSHU <anshu191001@gmail.com>
2021-12-07 01:27:04 +0530

    Added Axios Implementation

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 var express = require("express");
+var axios = require('axios')
 var request = require("request");
+const { response } = require("express");
 var app = express();
 var port = process.env.port || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-    request("https://v2.jokeapi.dev/joke/Any", function(err, response, body) {
-        if(err) {
-            console.log(err);
-        } else {
-            data = JSON.parse(body);
-            res.render("getJokes",{data: data});
-        }
+
+app.get('/',(req,res)=>{
+    axios.get('https://v2.jokeapi.dev/joke/Any')
+    .then(resp=>{
+        const data = resp.data;
+        console.log(data);
+        res.render("getJokes",{data:data})
     })
-});
+    .catch(err=>{
+        console.log(err);
+    })
+})
+
+
 
 
 app.listen(port, function() {
