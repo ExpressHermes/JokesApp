a98703321254fbbaadcc5d924dda651ac425f519
kartikeyjangir <kartikeyjangir101001@gmail.com>
2021-12-10 22:59:00 +0530

    Added option to generate different genre of jokes

--- app.js
+++ app.js
@@ -3,54 +3,73 @@ var request = require("request");
 var app = express();
 var port = process.env.port || 3000;
 
-require('dotenv').config();
-
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get("/getJokes/:type", function (req, res) {
+  const jokeType = req.params.type;
+  if (jokeType === "dadJokes") {
     request(
-        {method: 'GET',
-        url: 'https://dad-jokes.p.rapidapi.com/random/joke',
+      {
+        method: "GET",
+        url: "https://dad-jokes.p.rapidapi.com/random/joke",
         headers: {
-          'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
-          'x-rapidapi-key': process.env.Jokes_API_KEY,
-          useQueryString: true
-        }
-        }, function(err, response, body) {
-        if(err) {
-            console.log(err);
+          "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
+          "x-rapidapi-key":
+            "0fd4d724ffmsh4ea8edb72ad20bdp186caejsn8a504fc2f046",
+          useQueryString: true,
+        },
+      },
+      function (err, response, body) {
+        if (err) {
+          console.log(err);
         } else {
-            data = {
-                body:JSON.parse(body).body[0],
-                urlName:"dadJokes"
-                };
-            res.render("getJokes",{data: data});
+          data = {
+            body: JSON.parse(body).body[0],
+            urlName: "dadJokes",
+          };
+          res.render("getJokes", { data: data });
         }
-    })
-});
-app.get("/chuck", function(req, res) {
+      }
+    );
+  } else if (jokeType === "chuck") {
     request(
-        {method: 'GET',
-        url: 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random',
+      {
+        method: "GET",
+        url: "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
         headers: {
-          accept: 'application/json',
-          'x-rapidapi-host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
-          'x-rapidapi-key': process.env.Jokes_API_KEY,
-          useQueryString: true
-        }}, function(err, response, body) {
-        if(err) {
-            console.log(err);
+          accept: "application/json",
+          "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+          "x-rapidapi-key":
+            "0fd4d724ffmsh4ea8edb72ad20bdp186caejsn8a504fc2f046",
+          useQueryString: true,
+        },
+      },
+      function (err, response, body) {
+        if (err) {
+          console.log(err);
         } else {
-            data = {
-                body:JSON.parse(body),
-                urlName:"chuckJokes"
-            }
-            res.render("getJokes",{data: data});
+          data = {
+            body: JSON.parse(body),
+            urlName: "chuckJokes",
+          };
+          res.render("getJokes", { data: data });
         }
-    })
+      }
+    );
+  }
 });
+// app.get("/", function(req, res) {
+//     request("https://v2.jokeapi.dev/joke/Any", function(err, response, body) {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             data = JSON.parse(body);
+//             res.render("getJokes",{data: data});
+//         }
+//     })
+// });
 
-app.listen(port, function() {
-   console.log("Server Started!");
+app.listen(port, function () {
+  console.log(`Server running at http://localhost:${port}/getJokes/dadJokes`);
 });
